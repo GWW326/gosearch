05b844b15ce9816e68b1faad6ad66f7758a6a713
geweiwen <1075623516@qq.com>
2020-08-04 20:00:44 +0800

    Initial commit

---
+++ src/main/java/com/govin/gosearch/GosearchApplication.java
@@ -0,0 +1,13 @@
+package com.govin.gosearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GosearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/govin/gosearch/GosearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govin.gosearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
